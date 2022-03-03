5e1c48ad7d545b5c681b19689ae0bb6547ac7d63
rohitmaheshwari <rohitkumar.balwani@gmail.com>
2022-03-04 01:18:38 +0500

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +30,77 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.',
+  // ];
+  //
+  // List<bool> answers = [
+  //   false,
+  //   true,
+  //   true,
+  // ];
+  // Question q1 =
+  //     Question('You can lead a cow down stairs but not up stairs.', false);
+  // List<Question> questionBook = [
+  //   Question('You can lead a cow down stairs but not up stairs.', false),
+  //   Question('Approximately one quarter of human bones are in the feet.', true),
+  //   Question('A slug\'s blood is green.', true),
+  // ];
+
+  // int questionNumber = 0;
+
+  void checkAnswer(bool userPickedAnswer) {
+    setState(() {
+      if (quizBrain.isFinished()) {
+        if (quizBrain.getQuestionAnswer() == userPickedAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        Alert(context: context, title: "rFLUTTER", desc: "Flutter is awesome.")
+            .show();
+
+        quizBrain.reset(); // reset the question number
+
+        scoreKeeper = []; // emptying the store keeper
+      } else {
+        if (quizBrain.getQuestionAnswer() == userPickedAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  // void checkIfFinished() {
+  //   if (quizBrain.isFinished()) {
+  //     Alert(context: context, title: "rFLUTTER", desc: "Flutter is awesome.")
+  //         .show();
+  //
+  //     quizBrain.reset(); // reset the question number
+  //
+  //     scoreKeeper = []; // emptying the store keeper
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +113,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                // questions.first,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +138,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                // checkIfFinished();
+                checkAnswer(true);
               },
             ),
           ),
@@ -79,12 +157,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                // checkIfFinished();
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
